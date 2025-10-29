ee0c95385644badb397af21ed85bd225bf62cd30
Weltmacht <corydm@verizon.net>
2025-10-29 16:24:28 -0500

    accesors through get, for readability, added try catch to opendota calls

--- svc-test/main.py
+++ svc-test/main.py
@@ -14,57 +14,55 @@ def process():
 
     content = request.json
 
+    match_id = content.get("map", {}).get("matchid")
+
     if _player_team == '':
-        if "player" in content:
-            if "team_name" in content["player"]:
-                _player_team = content["player"]["team_name"]
-
-    if 'map' in content:
-        match_id = content["map"]["matchid"]
-        if _previous_known_state != content["map"]["game_state"]:
-            _previous_known_state = content["map"]["game_state"]
-            if content["map"]["game_state"] == "DOTA_GAMERULES_STATE_HERO_SELECTION":
-                pass
-            if content["map"]["game_state"] == "DOTA_GAMERULES_STATE_STRATEGY_TIME":
-                pass
-            if content["map"]["game_state"] == "DOTA_GAMERULES_STATE_SHOWCASE_TIME":
-                pass
-            if content["map"]["game_state"] == "DOTA_GAMERULES_STATE_GAME_IN_PROGRESS":
-                pass
-            if content["map"]["game_state"] == "DOTA_GAMERULES_STATE_POST_GAME":
+        _player_team = content.get("player", {}).get("team_name", '')
+    
+    if _previous_known_state != content["map"]["game_state"]:
+        _previous_known_state = content["map"]["game_state"]
+        if content["map"]["game_state"] == "DOTA_GAMERULES_STATE_HERO_SELECTION":
+            pass
+        if content["map"]["game_state"] == "DOTA_GAMERULES_STATE_STRATEGY_TIME":
+            pass
+        if content["map"]["game_state"] == "DOTA_GAMERULES_STATE_SHOWCASE_TIME":
+            pass
+        if content["map"]["game_state"] == "DOTA_GAMERULES_STATE_GAME_IN_PROGRESS":
+            pass
+        if content["map"]["game_state"] == "DOTA_GAMERULES_STATE_POST_GAME":
+            try:
                 response = requests.get(url = f"https://api.opendota.com/api/matches/{match_id}")
-                if response.status_code == 200:
-                    data = json.loads(response.text)   
+                response.raise_for_status()
+                data = json.loads(response.text) 
+                for player in data["players"]:
+                    print("PSlot: ", player["player_slot"])
+                    if "personaname" in player:
+                        print("Persona name: ", player["personaname"])
+                    if "name" in player:
+                        print("Player name: ", player["name"])
+                    if "rank_tier" in player:
+                        print("Rank: ", player["rank_tier"])
+                    if "lane_pos" in player:
+                        print("Position: ", player["lane_pos"])
+                endmatch(content["map"]["win_team"])
 
-                    print(data["players"])
-
-                    for player in data["players"]:
-                        print("PSlot: ", player["player_slot"])
-                        if "personaname" in player:
-                            print("Persona name: ", player["personaname"])
-                        if "name" in player:
-                            print("Player name: ", player["name"])
-                        if "rank_tier" in player:
-                            print("Rank: ", player["rank_tier"])
-                        if "lane_pos" in player:
-                            print("Position: ", player["lane_pos"])
-
-                endmatch(content)
-
+            except requests.RequestException as e:
+                print(f"OpenDota Request failed: {e}")
+                return None
+            
     return jsonify(message={"state": "succeeded"}, status=200), 200
 
-def endmatch(match: dict):
+def endmatch(win_team):
     global _player_team
     global _previous_known_state
 
-    print("Game winner: ", match["map"]["win_team"])
-    if match["map"]["win_team"] == match["map"]["win_team"]:
+    print("Game winner: ",win_team)
+    if _player_team == win_team:
         print("Well it looks like you got carried...")
 
     # reset match state
     _player_team = ''
     _previous_known_state = ''
-    pass
 
 if __name__ == "__main__":
     app.run()
